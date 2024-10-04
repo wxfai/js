d006505e25568ca95ca1fd27126e5f7d1255bcea
wxuefei <xuefei.wu@hotmail.com>
2024-10-04 10:42:14 +0800

    fixed commands list

--- src/com/xf/js/macho.java
+++ src/com/xf/js/macho.java
@@ -5,43 +5,81 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.List;
 
 public class macho {
+	static int MH_MAGIC_64			= 0xfeedfacf; /* the 64-bit mach magic number */
+	static int MH_EXECUTE			= 0x2;		/* demand paged executable file */
+
+    static int MH_NOUNDEFS			= 0x00000001;		// Flags (e.g., MH_NOUNDEFS = 0x00000001)
+    static int CPU_TYPE_X86_64		= 0x01000007;
+    static int CPU_SUBTYPE_X86_64_ALL	= 0x00000003;
+//
+	static int VM_PROT_READ			= 1;
+	static int VM_PROT_WRITE		= 2;
+	static int VM_PROT_EXECUTE		= 4;
+	
+	static int LC_REQ_DYLD 			= 0x80000000;
+	static int LC_SYMTAB			= 0x02;
+	static int LC_DYSYMTAB			= 0x0b;
+	static int LC_LOAD_DYLIB		= 0x0c;
+	static int LC_LOAD_DYLINKER		= 0x0e;
+	static int LC_SEGMENT_64 		= 0x19;
+	static int LC_DYLD_INFO			= 0x22;
+	static int LC_DYLD_INFO_ONLY	= LC_DYLD_INFO | LC_REQ_DYLD;
+	static int LC_MAIN 				= 0x80000028;
 
     public static void main(String[] args) {
         String outputFile = "out/hello";
-        segment_command_64 s = new segment_command_64("TEXT", LC_SEGMENT_64);
-        section_64 sec = new section_64("_text","_TEXT");
-        section_64 sec2 = new section_64("__unwind_info","_TEXT");
-        s.appendSection(sec);
-        s.appendSection(sec2);
-        s.toArray();
-        if(1>0)
-        return;
+        
+        macho_header64 m = new macho_header64();
+        
+        segment_command_64 pagezero = new segment_command_64("__PAGEZERO", LC_SEGMENT_64);
+        pagezero.vmsize =  0x100000000l;
 
-        try (FileOutputStream fos = new FileOutputStream(outputFile)) {
-            byte[] text_text = makeLCSegment();
-            byte lc_segment_64[] = makeSegment();
-            
-            byte[] mainCommand = makeLCMainCommand();
-            byte[] code = makeCode();
-            
-            byte[] dataSeg = "Hello, World!\n".getBytes();
+        segment_command_64 text = new segment_command_64("__TEXT", LC_SEGMENT_64);
+        section_64 sec = new section_64("__text","__TEXT");
+        section_64 sec2 = new section_64("__unwind_info","__TEXT");
+        text.appendSection(sec);
+        text.appendSection(sec2);
+        text.vmaddr = 0x100000000l;
+        text.vmsize = 0x1000;
+        text.filesize = 4096;
+        text.maxprot = 7;
+        text.initprot = 5;
+        //sec.
+        //text.toArray();
 
-            byte[] machoHeader = makeMachoHeader();
-            
-            fos.write(machoHeader);
-            fos.write(lc_segment_64);
-            //fos.write(text_text);
-            fos.write(mainCommand);
+        segment_command_64 data = new segment_command_64("__DATA", LC_SEGMENT_64);
+        section_64 data_sec = new section_64("__data","__DATA");
+        data.appendSection(data_sec);
 
-            fos.write(code);
-            fos.write(dataSeg);            // 写入字符串 "Hello, World!\n" 到数据段
+        segment_command_64 linkedit = new segment_command_64("__LINKEDIT", LC_SEGMENT_64);
 
-            System.out.println("Mach-O file with 'Hello, World!' created at " + outputFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        symtab_command symtab = new symtab_command();
+        dysymtab_command dysymtab = new dysymtab_command();
+        entry_point_command main_entry = new entry_point_command(0,0);
+        dyld_info_command ld_info = new dyld_info_command();
+        dylinker_command linker = new dylinker_command("/usr/lib/dyld");
+        dylib_command lib = new dylib_command("/usr/lib/libSystem.B.dylib");
+
+        m.appendCommand(pagezero);
+        m.appendCommand(text);
+        m.appendCommand(data);
+        m.appendCommand(linkedit);
+        m.appendCommand(symtab);
+        m.appendCommand(dysymtab);
+        m.appendCommand(main_entry);
+        m.appendCommand(ld_info);
+        m.appendCommand(linker);
+        m.appendCommand(lib);
+        
+        pagezero.cmdsize = 72;
+        m.writeFile(outputFile);
+    }
+    static void pln(String log) {
+    	System.out.println(log);
     }
     
     private static byte[] makeCode() {
@@ -86,113 +124,6 @@ public class macho {
 		return code;
 	}
 
-	private static byte[] makeLCSegment() {
-		segment_command_64 s = new segment_command_64("__TEXT", LC_SEGMENT_64);
-        s.cmdsize = 56 + 8+8;
-        s.vmaddr = 0x0100000000L;	// vmaddr (virtual memory address placeholder)
-        s.vmsize = 0x1000; 			// size for small code
-        s.fileoff = 0;	//fileoff (file offset)//0x100
-        // filesize (file size)
-        long codeSize = 1024;  // 假设代码段的大小为 1024 字节
-        //long fileSize = 4096; //1024 + 13;  // 假设代码段为 1024 字节，字符串为 13 字节
-        s.filesize = 32 + 72 + 24 + codeSize;  // 文件头 + 段命令 + LC_MAIN + 代码段
-
-        s.maxprot =  VM_PROT_READ | VM_PROT_EXECUTE; 
-        s.initprot = VM_PROT_READ | VM_PROT_EXECUTE;
-        s.nsects = 0;
-        s.flags = 0;
-        
-        byte[] dat = s.toArray();
-		return dat;
-	}
-    
-	private static byte[] makeLCMainCommand() {
-        // Load command LC_MAIN
-        ByteBuffer mainCommand = ByteBuffer.allocate(24);
-        mainCommand.order(ByteOrder.LITTLE_ENDIAN);
-        
-        mainCommand.putInt(LC_MAIN);  // cmd, LC_MAIN = 0x80000028
-        mainCommand.putInt(24);          // cmdsize
-
-        long entryOffset = 32 + 72 + 24;
-        //entryOffset = 0x100; 
-        mainCommand.putLong(entryOffset);  // // Entry point offset from the beginning of the Mach-O file, Adjust for actual entry point offset
-        // Stack size
-        mainCommand.putLong(0);      // Stack size (0 for default)
-
-        byte[] data = mainCommand.array();
-		return data;
-	}
-	private static byte[] makeMachoHeader() {
-        int MH_NOUNDEFS = 0x00000001;        		// Flags (e.g., MH_NOUNDEFS = 0x00000001)
-
-        int magic = 0xfeedfacf;       		 		// Magic number for 64-bit Mach-O (0xfeedfacf)
-    	int CPU_TYPE_X86_64 = 0x01000007;	        // CPU type (CPU_TYPE_X86_64 = 0x01000007)
-        int CPU_SUBTYPE_X86_64_ALL = 0x00000003;	// CPU subtype (CPU_SUBTYPE_X86_64_ALL = 0x00000003)
-        int MH_EXECUTE = 0x00000002;				// File type (MH_EXECUTE = 0x00000002, executable file)
-        int numCommands = 2;						// Number of load commands        // LC_SEGMENT_64 + LC_MAIN
-        int sizeOfLoadCommands = 56+16 + 24; 		// 56 bytes for LC_SEGMENT_64, 24 bytes for LC_MAIN
-        int flags = MH_NOUNDEFS;
-    	
-        ByteBuffer header = ByteBuffer.allocate(32);
-        header.order(ByteOrder.LITTLE_ENDIAN);
-        
-        header.putInt(magic);
-        header.putInt(CPU_TYPE_X86_64);
-        header.putInt(CPU_SUBTYPE_X86_64_ALL);
-        header.putInt(MH_EXECUTE);
-        header.putInt(numCommands);
-        header.putInt(sizeOfLoadCommands);
-        header.putInt(flags);
-        byte[] code = header.array();
-        return code;
-    }
-	private static byte[] makeSegment() {
-        // Load command LC_SEGMENT_64 for __TEXT segment
-        int cmdsize = 56 + 8+8;
-        ByteBuffer segmentCommand = ByteBuffer.allocate(cmdsize);
-        segmentCommand.order(ByteOrder.LITTLE_ENDIAN);
-
-        // LC_SEGMENT_64 = 0x19
-        segmentCommand.putInt(0x19);  // cmd
-        segmentCommand.putInt(cmdsize);    // cmdsize
-        segmentCommand.put("__TEXT".getBytes()); // segment name __TEXT, 16bytes
-        segmentCommand.position(24);  // Skip unused segment name bytes
-        // vmaddr (virtual memory address placeholder)
-        segmentCommand.putLong(0x0100000000L);
-        // vmsize (virtual memory size placeholder)
-        segmentCommand.putLong(0x1000); // size for small code
-        // fileoff (file offset)
-        segmentCommand.putLong(0);	//0x100
-        // filesize (file size)
-        long codeSize = 1024;  // 假设代码段的大小为 1024 字节
-        //long fileSize = 4096; //1024 + 13;  // 假设代码段为 1024 字节，字符串为 13 字节
-        long fileSize = 32 + 72 + 24 + codeSize;  // 文件头 + 段命令 + LC_MAIN + 代码段
-
-        segmentCommand.putLong(fileSize);  // filesize
-        
-//        segmentCommand.putLong(codeSize); // size for small code
-        // maxprot (read, write, execute)
-        segmentCommand.putInt(5); // VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXECUTE
-        // initprot (read, execute)
-        segmentCommand.putInt(5); // VM_PROT_READ | VM_PROT_EXECUTE
-        // Number of sections
-        segmentCommand.putInt(0);
-        // Flags
-        segmentCommand.putInt(0);		
-		return segmentCommand.array();
-	}
-	static int VM_PROT_READ			= 1;
-	static int VM_PROT_WRITE		= 2;
-	static int VM_PROT_EXECUTE		= 4;
-	
-	static int LC_SYMTAB			= 0x02;
-	static int LC_LOAD_DYLINKER		= 0x07;
-	static int LC_DYSYMTAB			= 0x0b;
-	static int LC_LOAD_DYLIB		= 0x0c;
-	static int LC_SEGMENT_64 		= 0x19;
-	static int LC_DYLD_INFO_ONLY	= 0x22;
-	static int LC_MAIN 				= 0x80000028;
 	
 	static class base{
         byte[] toArray() {
@@ -201,6 +132,7 @@ public class macho {
 	        	Field[] fields = super.getClass().getDeclaredFields();
 	//        	fields = this.getClass().getSuperclass().getDeclaredFields();
                 ByteBuffer buf = ByteBuffer.allocate(8192);
+                
                 buf.order(ByteOrder.LITTLE_ENDIAN);
 	        	for(Field f:fields) {
 	        		Class<?> type = f.getType();
@@ -219,15 +151,72 @@ public class macho {
 	                    buf.put(name); 
 //	                    buf.position(24);
 	        		}
+	        		else if(type == byte[].class) {
+	                    buf.put((byte[])f.get(this));
+	        		}
 	        	}
-	        	bytes = buf.array();
+	        	bytes = new byte[buf.position()];
+	        	System.arraycopy(buf.array(), 0, bytes, 0, buf.position());	        	
         	}
         	catch(Exception e) {
         		System.out.println(e.toString());
         	}
         	return bytes;
         }
+        int align8(int n) {
+        	if(n % 8 > 0)
+        		n += 8 - n % 8;
+			return n;
+        }
+        int align16(int n) {
+        	if(n % 16 > 0)
+        		n += 16 - n % 16;
+			return n;
+        }
 	}
+	
+	static class macho_header64 extends base {
+		int magic		= MH_MAGIC_64;       		// Magic number for 64-bit Mach-O (0xfeedfacf)
+    	int cputype		= CPU_TYPE_X86_64;	    	// CPU type (CPU_TYPE_X86_64 = 0x01000007)
+        int cpusubtype	= CPU_SUBTYPE_X86_64_ALL;	// CPU subtype (CPU_SUBTYPE_X86_64_ALL = 0x00000003)
+        int filetype	= MH_EXECUTE;				// File type (MH_EXECUTE = 0x00000002, executable file)
+        int ncmds		= 0;						// Number of load commands        // LC_SEGMENT_64 + LC_MAIN
+        int sizeofcmds	= 0; //56+16 + 24; 				// 56 bytes for LC_SEGMENT_64, 24 bytes for LC_MAIN
+        int flags		= MH_NOUNDEFS;
+    	int reserved	= 0;
+    	List<base> cmds = new ArrayList<base>();
+    	
+    	macho_header64(){
+    		
+    	}
+		public void appendCommand(base cmd) {
+        	cmds.add(cmd);
+        	ncmds ++;
+        	sizeofcmds += cmd.toArray().length; // cmd.cmdsize();	// add size of section
+        }
+        public void writeFile(String outputFile) {
+            try (FileOutputStream fos = new FileOutputStream(outputFile)) {
+                byte[] machoHeader = toArray();
+                 
+                byte[] code = makeCode();
+                byte[] dataSeg = "Hello, World!\n".getBytes();
+
+                fos.write(machoHeader);
+                for(base cmd:cmds) {
+                    byte[] bytes = cmd.toArray();
+                    fos.write(bytes); 
+                    pln("Write " + bytes.length +" bytes, " + cmd);
+                }
+
+                fos.write(code);
+                fos.write(dataSeg);            // 写入字符串 "Hello, World!\n" 到数据段
+
+                System.out.println("Mach-O file with 'Hello, World!' created at " + outputFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+		}   	
+    }
 	
 	static class segment_command_64 extends base{ /* for 64-bit architectures */
 		int cmd;        /* LC_SEGMENT_64 */
@@ -241,55 +230,31 @@ public class macho {
         int initprot;	/* initial VM protection */// initprot (read, execute) // VM_PROT_READ | VM_PROT_EXECUTE
         int nsects;		/* number of sections in segment */// Number of sections
         int flags;		/* flags */
-        section_64[] sections;
+    	List<section_64> sections = new ArrayList<section_64>();
         
         public segment_command_64(String _segname, int _cmd) {
             segname = _segname;
             cmd = _cmd;
             cmdsize = 72; // 0 section 
 		}
+    	
         public void appendSection(section_64 sec) {
-        	int new_num = nsects + 1;
-        	section_64[] ns = new section_64[new_num];
-        	for(int i=0;i<nsects;i++)
-            	ns[i] = sections[i];
-        	ns[nsects] = sec;
-        	nsects = new_num;
-        	sections = ns;
+        	sections.add(sec);
+        	nsects ++;
         	cmdsize += 80;	// add size of section
         }
+        public byte[] toArray() {
+        	byte[] bytes = super.toArray();
+            ByteBuffer buf = ByteBuffer.allocate(8192);
+            buf.put(bytes);
+            for(section_64 s:sections) {
+            	buf.put(s.toArray());
+            }
+        	bytes = new byte[buf.position()];
+        	System.arraycopy(buf.array(), 0, bytes, 0, buf.position());
 
-//		byte[] toArray() {
-//        	ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//        	java.io.DataOutputStream dos = new java.io.DataOutputStream(baos);
-//
-//        	cmdsize = 72;
-//            ByteBuffer buf = ByteBuffer.allocate(8192);
-//            buf.order(ByteOrder.LITTLE_ENDIAN);
-//
-//            buf.putInt(cmd); 
-//            buf.putInt(cmdsize);
-//            buf.put(segname.getBytes()); 
-//            buf.position(24);  // Skip unused segment name bytes
-//            buf.putLong(vmaddr);
-//            buf.putLong(vmsize); // size for small code
-//            buf.putLong(fileoff);	//0x100
-//            // filesize (file size)
-////            long codeSize = 1024;  // 假设代码段的大小为 1024 字节
-//            //long fileSize = 4096; //1024 + 13;  // 假设代码段为 1024 字节，字符串为 13 字节
-////            long fileSize = 32 + 72 + 24 + codeSize;  // 文件头 + 段命令 + LC_MAIN + 代码段
-//
-//            buf.putLong(fileSize);  // filesize
-//            buf.putInt(maxprot);
-//            buf.putInt(initprot);
-//            buf.putInt(nsects);
-//            buf.putInt(flags);		
-//        	
-//            byte[] data = new byte[cmdsize];
-//            buf.flip();
-//            buf.get(data);
-//        	return data;
-//        }
+        	return bytes;
+        }
 	}
 
 	static class symtab_command extends base{
@@ -307,10 +272,8 @@ public class macho {
 	}
 	
 	static class section_64 extends base{ /* for 64-bit architectures */
-//		byte[]		sectname = new byte[16];	/* name of this section */
-//		byte[]		segname = new byte[16];	/* segment this section goes in */
-		String		sectname;	/* name of this section */
-		String		segname;	/* segment this section goes in */
+		String		sectname;	/* name of this section, byte[16] */
+		String		segname;	/* segment this section goes in, byte[16] */
 		long	addr;		/* memory address of this section */
 		long	size;		/* size in bytes of this section */
 		int	offset;		/* file offset of this section */
@@ -329,7 +292,7 @@ public class macho {
 	}
 
 	
-	class dysymtab_command extends base{
+	static class dysymtab_command extends base{
 	    int cmd;		/* LC_DYSYMTAB */
 	    int cmdsize;	/* sizeof(class dysymtab_command) */
 	    int ilocalsym;	/* index to local symbols */
@@ -376,53 +339,48 @@ public class macho {
 	    	cmdsize = 48;
 	    }
 	};
-//	union lc_str {
-//		uint32_t	offset;	/* offset to the string */
-//	#ifndef __LP64__
-//		char		*ptr;	/* pointer to the string */
-//	#endif 
-//	};
 
 	static class dylinker_command extends base{
 		int	cmd;		/* LC_ID_DYLINKER, LC_LOAD_DYLINKER or
 						   LC_DYLD_ENVIRONMENT */
-		int	cmdsize;	/* includes pathname string, 16 bytes align? */
+		int	cmdsize;	/* includes pathname string, 8 bytes align? */
 //		lc_str name;
-		int name;		/* dynamic linker's path name */
-		
-		dylinker_command() {
+		int name_offset;		/* dynamic linker's path name */
+	    byte[] linker_path;
+	    byte[] align_zero;
+
+		dylinker_command(String linker) {
 			cmd = LC_LOAD_DYLINKER;
-//			cmdsize = 
+			cmdsize = 12 + linker.length();
+			int size = align8(cmdsize);
+			align_zero = new byte[size-cmdsize];
+			cmdsize = size;
+			name_offset = 12;
+			linker_path = linker.getBytes();
 		}
 	};
 
-	static class dylib extends base{
-		int name;			/* lc_str library's path name */
-	    int timestamp;			/* library's build time stamp */
-	    int current_version;		/* library's current version number */
-	    int compatibility_version;	/* library's compatibility vers number*/
-	    
-	    dylib() {
-	    	
-	    }
-	};
 
-	/*
-	 * A dynamically linked shared library (filetype == MH_DYLIB in the mach header)
-	 * contains a dylib_command (cmd == LC_ID_DYLIB) to identify the library.
-	 * An object that uses a dynamically linked shared library also contains a
-	 * dylib_command (cmd == LC_LOAD_DYLIB, LC_LOAD_WEAK_DYLIB, or
-	 * LC_REEXPORT_DYLIB) for each library it uses.
-	 */
 	static class dylib_command extends base{
 		int	cmd;			/* LC_ID_DYLIB, LC_LOAD_{,WEAK_}DYLIB,
 						   		LC_REEXPORT_DYLIB */
 		int	cmdsize;		/* includes pathname string */
-		dylib dylib;		/* the library identification */
-		
-		dylib_command() {
+//		dylib dylib;		/* the library identification */
+		int name_offset;			/* lc_str library's path name, 8bytes align */
+	    int timestamp;				/* library's build time stamp */
+	    int current_version;		/* library's current version number */
+	    int compatibility_version;	/* library's compatibility vers number*/
+	    byte[] lib_path;
+	    byte[] align_zero;
+	    
+		dylib_command(String _lib_path) {
 			cmd = LC_LOAD_DYLIB;
-//			cmdsize = 
+			cmdsize = 24 + _lib_path.length();
+			int size = align8(cmdsize);
+			align_zero = new byte[size-cmdsize];
+			cmdsize = size;
+			name_offset = 24;
+			lib_path = _lib_path.getBytes();
 		}
 	};
 	
